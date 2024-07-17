d694af90846e7df8324aaa6c7fa9b5eafbd3c5fc
0fzc <3557248010@qq.com>
2024-07-17 11:05:46 +0800

    异常处理 try catch

---
+++ tryCatch.java
@@ -0,0 +1,30 @@
+package com.hspedu.Homework;
+
+import java.util.Scanner;
+//      异常处理方法   try——catch
+/**
+ * @author 冯振聪
+ * @version 1.0
+ */
+public class fsf {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int num=0;
+        String str="";
+
+        while(true){
+            System.out.println("请输入一个整数");
+            str=myScanner.next();//初步定义
+            try {
+                num=Integer.parseInt(str);//如果抛出异常，则try后面的不再执行,执行catch
+                System.out.println("输入的值为"+num);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("输入错误，请输入一个整数");
+//                throw new RuntimeException(e);   这句话会抛出红字
+            }
+
+
+        }
+    }
+}
